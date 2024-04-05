039872207c376b2d302e9ca9237353ca04adc114
Hynek Handlíř <hyndoman@gmail.com>
2024-04-05 21:00:20 +0200

    implementace controlleru

--- BrnoJam2024/Assets/Scripts/Player/Player.cs
+++ BrnoJam2024/Assets/Scripts/Player/Player.cs
@@ -4,4 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public Transform CameraPosition => _cameraPosition;
+	public Rigidbody Rigidbody => _rigidbody;
+
+	[SerializeField] private Transform _cameraPosition;
+	[SerializeField] private Rigidbody _rigidbody;
 }
